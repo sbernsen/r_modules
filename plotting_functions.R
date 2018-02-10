7970a644c2c96f00e72304aa686d36ae7055aaa7
sbernsen <stevenbernsen@gmail.com>
2018-02-10 17:54:41 -0500

    plotting updates

--- plotting_functions.R
+++ plotting_functions.R
@@ -26,7 +26,7 @@ ts_norm <- function(df, k)
   return(df)
 }
 
-ts_shifts <- function( df )
+ts_yshifts <- function( df )
 {
   m <- dim(df)[1]
   n <- dim(df)[2]
@@ -43,24 +43,67 @@ ts_shifts <- function( df )
   
 }
 
-plot_geophone_wiggles <- function(time_vec, df, line_fill )
+plot_geophone_wiggles <- function( time_vec, df, line_fill, vert, hw)
 {
   
-  df <- ts_shifts(df) 
+  df <- ts_yshifts(df) 
   amp_shift <- df$amp_shift
   df <- df$ts_shift 
   
-  dev.new(width = 8, height = 12, units = "in")
-  plot( df[,1], time_vec, type = "l", xlim = c(min(amp_shift), max(amp_shift) ),
-       ylim = ylims, xlab = "", ylab = "", xaxt = "n", 
-       lwd = 0.5 )
-  pm_polygon_fill(time_vec, df[,1], amp_shift[1], line_fill)
-  
-  for(i in 2:length(amp_shift) )
+
+  if( vert )
   {
-    lines( df[,i], time_vec, lwd = 0.5)
-    pm_polygon_fill(time_vec, df[,i], amp_shift[i], line_fill)
-  }
+    dev.new(width = hw[2], height = hw[1], units = "in")
+    plot( df[,1], time_vec, type = "l", xlim = c(min(amp_shift), max(amp_shift) ),
+         ylim = c(max(time_vec), min(time_vec) ), xlab = "", ylab = "Two Way Travel Time (s)", xaxt = "n", 
+         lwd = 1.5 )
+
+    if(line_fill[1] != "none" )
+    {
+      pm_polygon_fill(time_vec, df[,1], amp_shift[1], line_fill)
+      for(i in 2:length(amp_shift) )
+      {
+        lines( df[,i], time_vec, lwd = 1.5)
+        pm_polygon_fill(time_vec, df[,i], amp_shift[i], line_fill)
+      }
+    }else 
+    { 
+      for(i in 2:length(amp_shift) )
+      {
+        lines( df[,i], time_vec, lwd = 1.5)
+      }
+    }
+  }else
+  {
+    dev.new(width = 12, height = 4, units = "in")
+    plot( time_vec, df[,1], type = "l", ylim = c(min(amp_shift), max(amp_shift) ),
+         xlim = c(min(time_vec), max(time_vec) ), xlab = "Time (s)", ylab = "Distance from CMP", 
+         lwd = 1.5, main= "T2 - CMP" )
   
+    if(line_fill[1] != "none" )
+    {
+      pm_polygon_fill(df[,1], time_vec, amp_shift[1], line_fill)
+      for(i in 2:length(amp_shift) )
+      {
+        lines( time_vec, df[,i], lwd = 1.5)
+        pm_polygon_fill(df[,i], time_vec, amp_shift[i], line_fill)
+      }
+    }else 
+    {  
+      for(i in 2:length(amp_shift) )
+      {
+        lines( time_vec, df[,i], lwd = 1.5)
+      }
+    
+    }
+  }
+
+#if( !is.null(direct_wave) && vert )
+#  {
+#    lines( amp_shift, direct_wave, col = "green4", lwd = 1.5)
+#  }else if( !is.null(direct_wave) )
+#  {
+#    lines( direct_wave, amp_shift, col = "green4", lwd = 1.5) 
+#  }
 }
 
